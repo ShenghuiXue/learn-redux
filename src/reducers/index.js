15bfd05e336d9d571e673069c1ce6766d0e5177a
ShenghuiXue <shenghuixue@gmail.com>
2021-01-07 22:32:35 -0600

    master: complete

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songReducer = () => {
+  return [
+    { title: 'No Scurbs', duration: '4:05'},
+    { title: 'Macarena', duration: '2:30'},
+    { title: 'All star', duration: '3:05'},
+    { title: 'As long as you love me', duration: '4:05'}
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  } 
+
+  return selectedSong;
+}
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer
+});
